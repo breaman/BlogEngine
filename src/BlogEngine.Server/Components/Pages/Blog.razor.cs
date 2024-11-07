01f7d4f73e38b3922f6bcf1c00c66650b70f6811
Michael Stokesbary <mike@stokesbary.me>
2024-11-06 18:27:27 -0800

    Just a small branch that adds Markdig for display. - Also added "order by descending" for the main blog landing page.

--- src/BlogEngine.Server/Components/Pages/Blog.razor.cs
+++ src/BlogEngine.Server/Components/Pages/Blog.razor.cs
@@ -10,7 +10,9 @@ public partial class Blog : ComponentBase
 
     protected override async Task OnInitializedAsync()
     {
-        var allPosts = await DbContext.Posts.AsNoTracking().Where(p => p.IsPublished).ToListAsync();
+        var allPosts = await DbContext.Posts.AsNoTracking().Where(p => p.IsPublished)
+            .OrderByDescending(p => p.PublishDate).ToListAsync();
+        
         foreach (var post in allPosts)
         {
             _posts.Add(post.ToDto());
